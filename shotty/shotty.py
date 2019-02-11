dc3f155eff0e500ffc7a46c7fcf78d1d18c74221
Isah Yusuf <isahyusuf@Isahs-MacBook-Pro.local>
2019-02-11 11:34:28 -0600

    added new files

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,21 @@
 import boto3
+import click
 
-if --name-- == '--main--':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource( 'ec2' )
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource( 'ec2' )
 
-    for i in ec2.instance.all():
-        print(i)
+@click.command()
+def list_instances():
+    "list EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
+        
+if __name__ == '__main__':
+    list_instances()
